16b34f9afdc5ec5119bf4bc02b0d63d950fd56f7
ali sait bilgi <alisaitbilgi@gmail.com>
2016-11-17 15:50:09 +0200

    Update IyteImmutableList.scala

--- HW03/IyteImmutableList.scala
+++ HW03/IyteImmutableList.scala
@@ -1,22 +1,22 @@
-sealed abstract class IyteImmutableList{
-  def add(x : Int) :IyteImmutableList = new Cons(x, this) 
-}
 
-object Nil extends IyteImmutableList{
-  override def toString = ""
-}
-
-class Cons(head: Int, tail: IyteImmutableList) extends IyteImmutableList{
-  
-  override def toString() : String = {
-    if(tail != Nil)
-    head + "," + tail.toString()
-    else
-    head + tail.toString()
+  sealed abstract class IyteImmutableList {
+    def add(x: Int): IyteImmutableList = new Cons(x, this)
   }
-}
 
-object IyteImmutableList
-{
-  def apply() = Nil
-}
+  object Nil extends IyteImmutableList {
+    override def toString = ""
+  }
+
+  class Cons(head: Int, tail: IyteImmutableList) extends IyteImmutableList {
+
+    override def toString(): String = {
+      if (tail != Nil)
+        head + "," + tail.toString()
+      else
+        head + tail.toString()
+    }
+  }
+
+  object IyteImmutableList {
+    def apply(): IyteImmutableList = Nil
+  }
